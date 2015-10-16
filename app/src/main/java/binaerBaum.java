aebe24c3038aebd774f4ad6a1cf14cec667d03b4
felixdechet <felix.dechet@gmx.de>
2015-10-16 14:45:14 +0200

    updated binaerBaum.java

--- app/src/main/java/binaerBaum.java
+++ app/src/main/java/binaerBaum.java
@@ -3,10 +3,20 @@
  */
 public class binaerBaum {
 
+    public int anzahl;
+    public static String name;
+
     public void main (String[] args) {
         System.out.print("lol");
     }
 
+    public binaerBaum () {
+        this.binaerBaum (5, "default");
+    }
 
+    public binaerBaum (int zahl, String name) {
+        anzahl = zahl;
+        this.name = name;
+    }
 
 }
